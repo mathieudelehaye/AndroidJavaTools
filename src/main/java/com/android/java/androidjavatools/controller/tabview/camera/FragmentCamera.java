47126ad3ee23c44b120520da7b4670b92e6064c7
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-04-20 07:10:12 +0100

    Reusable camera fragment moved to library

---
+++ src/main/java/com/android/java/androidjavatools/controller/tabview/camera/FragmentCamera.java
@@ -0,0 +1,172 @@
+//  FragmentCamera.java
+//
+//  Created by Mathieu Delehaye on 17/12/2022.
+//
+//  AndroidJavaTools: A framework to develop Android apps in Java.
+//
+//  Copyright © 2022 Mathieu Delehaye. All rights reserved.
+//
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+//  Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+//  <https://www.gnu.org/licenses/>.
+
+package com.android.java.androidjavatools.controller.tabview.camera;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import com.android.java.androidjavatools.Helpers;
+import com.android.java.androidjavatools.databinding.FragmentCameraBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+public abstract class FragmentCamera extends Fragment {
+    protected FragmentCameraBinding mBinding;
+    protected Context mContext;
+    protected boolean mIsViewVisible = false;
+    private ListenableFuture<ProcessCameraProvider> mCameraProviderFuture;
+    protected abstract void defineCameraButtonBehaviour(ImageCapture imageCapture);
+    private static final int PERMISSION_REQUEST_CAMERA = 1;
+
+    @Override
+    public View onCreateView(
+        LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState
+    ) {
+        mBinding = FragmentCameraBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Log.v("BeautyAndroid", "Camera view created at timestamp: "
+            + Helpers.getTimestamp());
+
+        super.onViewCreated(view, savedInstanceState);
+
+        mContext = view.getContext();
+
+        mCameraProviderFuture = ProcessCameraProvider.getInstance(mContext);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults) {
+
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                Toast.makeText(mContext, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        if (isVisibleToUser) {
+            mIsViewVisible = true;
+            Log.d("BeautyAndroid", "Camera view becomes visible");
+
+            requestCamera();
+        } else {
+            mIsViewVisible = false;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void requestCamera() {
+        if (mContext != null && ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            var permissionsToRequest = new ArrayList<String>();
+            permissionsToRequest.add(Manifest.permission.CAMERA);
+
+            requestPermissions(
+                permissionsToRequest.toArray(new String[0]),
+                PERMISSION_REQUEST_CAMERA);
+        } else {
+            startCamera();
+        }
+    }
+
+    private void bindCameraPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        PreviewView previewView = mBinding.previewCamera;
+        previewView.setPreferredImplementationMode(PreviewView.ImplementationMode.SURFACE_VIEW);
+
+        var preview = new Preview.Builder().build();
+
+        var cameraSelector = new CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build();
+
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+
+        var imageCapture =
+            new ImageCapture.Builder()
+                .setTargetRotation(previewView.getDisplay().getRotation())
+                .setTargetResolution(new Size(480, 640))
+                .build();
+
+        defineCameraButtonBehaviour(imageCapture);
+
+        // avoid having too many use cases, when switching back to the camera screen
+        cameraProvider.unbindAll();
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+    }
+
+    public void startCamera() {
+        mCameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = mCameraProviderFuture.get();
+                bindCameraPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Toast.makeText(mContext, "Error starting camera " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }, ContextCompat.getMainExecutor(mContext));
+    }
+}
