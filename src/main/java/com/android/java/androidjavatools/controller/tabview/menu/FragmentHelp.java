5e3fd5ba084658d783e5ac8f89a2461bc3e5f9b4
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-04-20 06:28:21 +0100

    Other reusable menu fragments moved to library

---
+++ src/main/java/com/android/java/androidjavatools/controller/tabview/menu/FragmentHelp.java
@@ -0,0 +1,52 @@
+//
+//  FragmentHelp.java
+//
+//  Created by Mathieu Delehaye on 19/01/2023.
+//
+//  AndroidJavaTools: A framework to develop Android apps in Java.
+//
+//  Copyright © 2023 Mathieu Delehaye. All rights reserved.
+//
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+//  Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+//  <https://www.gnu.org/licenses/>.
+
+package com.android.java.androidjavatools.controller.tabview.menu;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.android.java.androidjavatools.Helpers;
+import com.android.java.androidjavatools.databinding.FragmentHelpBinding;
+
+public abstract class FragmentHelp extends Fragment {
+    protected FragmentHelpBinding mBinding;
+
+    @Override
+    public View onCreateView(
+        LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState
+    ) {
+        mBinding = FragmentHelpBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Log.v("BeautyAndroid", "Help view created at timestamp: "
+            + Helpers.getTimestamp());
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
