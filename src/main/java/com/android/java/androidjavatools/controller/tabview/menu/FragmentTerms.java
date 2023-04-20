9c3d15b41928fd8731f13b33010d59d22737a047
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2023-04-20 05:43:21 +0100

    Reusable Terms fragment moved to library

---
+++ src/main/java/com/android/java/androidjavatools/controller/tabview/menu/FragmentTerms.java
@@ -0,0 +1,52 @@
+//
+//  FragmentTerms.java
+//
+//  Created by Mathieu Delehaye on 19/01/2023.
+//
+//  AndroidJavaTools: A framework to develop Android apps in Java.
+//
+//  Copyright © 2022 Mathieu Delehaye. All rights reserved.
+//
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+//  Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+//  <https://www.gnu.org/licenses/>.
+
+package com.android.java.androidjavatools.controller.tabview.menu;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.android.java.androidjavatools.Helpers;
+import com.android.java.androidjavatools.databinding.FragmentTermsBinding;
+
+public abstract class FragmentTerms extends Fragment {
+    protected FragmentTermsBinding mBinding;
+
+    @Override
+    public View onCreateView(
+        LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState
+    ) {
+        mBinding = FragmentTermsBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        Log.v("BeautyAndroid", "Terms view created at timestamp: "
+            + Helpers.getTimestamp());
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
